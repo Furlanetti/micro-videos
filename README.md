943bfa81a3d84b0645cacd90007f24c26ea39630
Felipe Furlanetti <felipe.furlanetti1@gmail.com>
2022-08-08 18:23:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Montagem do ambienten de desenvolvimento (Docker e IDE)
+# Criar aplicação Node
+# Criar entidade de Categoria
+# Criar testes
+# Criar Casos de uso e repositório
+# Criar testes
+
+# Repetir para as outras entidades
+
+# Nest.js - Criação da API Rest
+# Integração com RabbitMQ e Enconder de video
+# Testes E2E
---
+++ src/category/domain/entities/category.spec.ts
@@ -0,0 +1,104 @@
+import Category from "./category";
+
+describe("Category test", () => {
+  it("constructor", () => {
+    let category = new Category({ name: "Movie" });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    let created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      description: "description",
+      is_active: false,
+      created_at,
+    });
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "description",
+      is_active: false,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "other description",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
+  });
+
+  test("getter of name field", () => {
+    const category = new Category({ name: "Movie" });
+
+    expect(category.name).toBe("Movie");
+  });
+
+  test("getter and setter of description field", () => {
+    const category_null = new Category({
+      name: "Movie",
+    });
+
+    expect(category_null.description).toBeNull();
+
+    const category = new Category({
+      name: "Movie",
+      description: "some description",
+    });
+
+    expect(category.description).toBe("some description");
+
+    category["description"] = "other description";
+    expect(category.description).toBe("other description");
+
+    category["description"] = undefined;
+    expect(category.description).toBeNull();
+  });
+
+  test("getter and setter of is_active", () => {
+    let category = new Category({ name: "Movie" });
+
+    expect(category.is_active).toBeTruthy();
+
+    category = new Category({ name: "Movie", is_active: false });
+    expect(category.is_active).toBeFalsy();
+
+    category = new Category({ name: "Movie", is_active: true });
+    expect(category.is_active).toBeTruthy();
+  });
+
+  test("getter of created_at", () => {
+    let category = new Category({ name: "Movie" });
+
+    expect(category.created_at).toBeInstanceOf(Date);
+
+    const created_at = new Date();
+    category = new Category({ name: "Movie", created_at });
+    expect(category.created_at).toBe(created_at);
+  });
+});
