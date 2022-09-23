367b27653178b53d0af32260cc105484a30e2933
Felipe Furlanetti <felipe.furlanetti1@gmail.com>
2022-09-23 15:04:58 -0300

    Fix test of category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,5 +1,5 @@
 import UniqueEntityId from "@seedwork/domain/value-objects/unique-entity-id.vo";
-import Entity from "@seedwork/entity/entity";
+import Entity from "@seedwork/domain/entity/entity";
 
 export interface Properties {
   name: string;
