9ef33f71921e34a05a6250f73994bdc54c5f051e
Felipe Furlanetti <felipe.furlanetti1@gmail.com>
2023-01-27 18:11:38 -0300

    Initial work on pagination for search in category repository

--- src/@seedwork/domain/repository/repository-contracts.ts
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -8,3 +8,63 @@ export interface RepositoryInterface<E extends Entity> {
   update(entity: E): Promise<void>;
   delete(id: string | UniqueEntityId): Promise<void>;
 }
+
+export type SortDirection = "asc" | "desc";
+
+export type SearchProps<Filter = string> = {
+  page?: number;
+  per_page?: number;
+  sort?: string;
+  sort_dir?: SortDirection;
+  filter?: Filter;
+};
+
+export class SearchParams {
+  protected _page: number;
+  protected _per_page = 15;
+  protected _sort: string | null;
+  protected _sort_dir: SortDirection | null;
+  protected _filter: Filter | null;
+
+  constructor(props: SearchProps = {}) {
+    this.page = props.page;
+    this.per_page = props.per_page;
+    this.sort = props.sort;
+    this.sort_dir = props.sort_dir;
+    this.filter = props.filter;
+  }
+  get per_page() {
+    return this._per_page;
+  }
+
+  set per_page(value: number) {}
+  get sort() {
+    return this._sort;
+  }
+
+  set sort(value: string | null) {}
+
+  set sort_dir(value: SortDirection | null) {}
+  get sort_dir() {
+    return this._sort_dir;
+  }
+
+  set filter(value: Filter | null) {}
+  get filter() {
+    return this._filter;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  set page(value: number) {}
+}
+
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  SearchInput,
+  SearchOutput
+> extends RepositoryInterface<E> {
+  search(props: SearchInput): Promise<SearchOutput>;
+}
--- src/category/domain/repository/category.repository.ts
+++ src/category/domain/repository/category.repository.ts
@@ -1,4 +1,4 @@
-import { RepositoryInterface } from "@seedwork/domain/repository/repository-contracts";
+import { SearchableRepositoryInterface } from "@seedwork/domain/repository/repository-contracts";
 import Category from "../entities/category";
 
-export default interface CategoryRepository extends RepositoryInterface<Category>{}
+export default interface CategoryRepository extends SearchableRepositoryInterface<Category, any,any>{}
