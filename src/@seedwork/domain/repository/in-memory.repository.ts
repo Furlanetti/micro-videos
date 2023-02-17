6b8439734374ab07fd62c56f9447423f60e58584
Felipe Furlanetti <felipe.furlanetti1@gmail.com>
2023-02-17 14:13:19 -0300

    More work on repository

--- src/@seedwork/domain/repository/in-memory.repository.ts
+++ src/@seedwork/domain/repository/in-memory.repository.ts
@@ -4,6 +4,8 @@ import uniqueEntityIdVo from "../value-objects/unique-entity-id.vo";
 import {
   RepositoryInterface,
   SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
 } from "./repository-contracts";
 
 export abstract class InMemoryRepository<E extends Entity>
@@ -52,9 +54,45 @@ export abstract class InMemoryRepository<E extends Entity>
 
 export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
-  implements SearchableRepositoryInterface<E, any, any>
+  implements SearchableRepositoryInterface<E>
 {
-  search(props: any): Promise<any> {
-    throw new Error("needs implementation");
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemsSorted = await this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sort_dir,
+    );
+    const itemsPaginated = await this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.per_page
+    );
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      current_page: props.page,
+      per_page: props.per_page,
+      sort: props.sort,
+      sort_dir: props.sort_dir,
+      filter: props.filter,
+    });
   }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null
+  ): Promise<E[]>;
+
+  protected abstract applySort(
+    items: E[],
+    sort: string | null,
+    sort_dir: string | null
+  ): Promise<E[]>;
+  protected abstract applyPaginate(
+    items: E[],
+    page: SearchParams["page"],
+    per_page: SearchParams["per_page"]
+  ): Promise<E[]>;
 }
